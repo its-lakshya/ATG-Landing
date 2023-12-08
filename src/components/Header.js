1c02415a78283596cf28271811df3194119625f2
Lakshya Kumar <kumarlakshya101@gmail.com>
2023-12-08 12:21:14 +0530

    build the header

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import logo from "../assets/logo.svg"
+import { IoSearch } from "react-icons/io5";
+import { IoMdArrowDropdown } from "react-icons/io";
+
+
+
+const Header = () => {
+    return (
+        <div className= "h-20 flex items-center justify-between px-24">
+            <img src={logo} alt="logo" className=""/>
+            <div className = "w-96  h-11 px-4 flex items-center justify-start gap-x-4 rounded-full bg-gray-200 text-gray-600 ">
+                <IoSearch/>
+                <input type="text" placeholder="search for your favourite group" className='text-black w-64 outline-none bg-gray-200 text-sm'/>
+            </div>
+            <div className='font-semibold flex gap-x-1'>Create account.
+            <span className='text-blue-600 flex items-center gap-x-1'> It's free <IoMdArrowDropdown className="text-gray-600"/></span>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
